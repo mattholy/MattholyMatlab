f3ee6cbae771de5cdfb1576aef97b97394a7bc65
Mattholy <smile.used@hotmail.com>
2017-03-17 15:11:47 +0800

    代码优化

--- mcb.m
+++ mcb.m
@@ -27,7 +27,7 @@ else
     disp('  Matlab版本正常，程序继续。');
     verCheck = 'y';
     disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
-    pause(3); 
+    pause(1); 
 end
 if verCheck == 'y'
     clc
@@ -72,45 +72,75 @@ if verCheck == 'y'
         disp('┃                             版本：1.0    创建于：2017/3/17   ┃');
         disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
         disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
-        fprintf('  已下载完成，用时%.2f秒  \n',time);
+        indexSize = dir('index.xlsx');
+        indexSize = indexSize.bytes/1024;
+        speed = indexSize/time;
+        fprintf('  已下载完成，用时%.2f秒,平均速度%.2fkb/s  \n',time,speed);
         disp('  读取目录中，请稍候...');
         disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
         [a,b,c]=xlsread('index.xlsx');
         delete ('index.xlsx');
         [items,y]=size(c);
-        pause(3);
-        clc
-        disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
-        disp('┃                马特浩倪Matlab代码部署系统                    ┃');
-        disp('┃                             版本：1.0    创建于：2017/3/17   ┃');
-        disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
-        disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
-        target = input('  请输入接收码（通常这是您的手机号或者QQ号） : ','s');
-        disp('  查询中，请稍候...');
-        disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
-        pause(3)
-        itemCount=0;
-        itemPos=[];
-        while itemCount==0
-            for i=(2:1:items)
-                for j=(4:1:5)
-                    mou=c{i,j};
-                    if strcmpi(c{i,j},target)
-                        itemCount=itemCount+1;
-                        itemPos=[itemPos,i];
-                    end
-                end
-            end
+        pause(2);
+        while(true)
             clc
             disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
             disp('┃                马特浩倪Matlab代码部署系统                    ┃');
             disp('┃                             版本：1.0    创建于：2017/3/17   ┃');
             disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
             disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
-            disp('  未查询到相关目录，请确认输入。');
             target = input('  请输入接收码（通常这是您的手机号或者QQ号） : ','s');
-            disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
-            pause;
+            k = str2double(target);
+            if isnan(k)
+            else
+                disp('  查询中，请稍候...');
+                disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
+                break
+            end
+        end
+        itemCount=0;
+        itemPos=[];
+        for i=(2:1:items)
+            for j=(4:1:5)
+                if c{i,j}==k
+                    itemCount=itemCount+1;
+                    itemPos=[itemPos,i];
+                end
+            end
+        end
+        while itemCount==0
+            for i=(2:1:items)
+                for j=(4:1:5)
+                    if c{i,j}==target
+                        itemCount=itemCount+1;
+                        itemPos=[itemPos,i];
+                    end
+                end
+            end
+            while(true)
+                clc
+                disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
+                disp('┃                马特浩倪Matlab代码部署系统                    ┃');
+                disp('┃                             版本：1.0    创建于：2017/3/17   ┃');
+                disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
+                disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
+                target = input('  请输入接收码（通常这是您的手机号或者QQ号） : ','s');
+                k = str2double(target);
+                if isnan(k)
+                    clc
+                    disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
+                    disp('┃                马特浩倪Matlab代码部署系统                    ┃');
+                    disp('┃                             版本：1.0    创建于：2017/3/17   ┃');
+                    disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
+                    disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
+                    disp('  请输入数字！');
+                    target = input('  请输入接收码（通常这是您的手机号或者QQ号） : ','s');
+                else
+                    disp('  查询中，请稍候...');
+                    disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
+                    break
+                end
+            end
         end
         itemOutput={};
         for i=(1:1:itemCount)
@@ -118,20 +148,27 @@ if verCheck == 'y'
                 itemOutput{i,j}=c{itemPos(i),j};
             end
         end
-        clc
-        disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
-        disp('┃                马特浩倪Matlab代码部署系统                    ┃');
-        disp('┃                             版本：1.0    创建于：2017/3/17   ┃');
-        disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
-        disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
-        disp('  当前目录：');
-        disp('   序号        名称');
-        for(i=1:1:itemCount)
-            fprintf('    %d          %s  \n',itemOutput{i,1},itemOutput{i,2});
+        while(true)
+            clc
+            disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
+            disp('┃                马特浩倪Matlab代码部署系统                    ┃');
+            disp('┃                             版本：1.0    创建于：2017/3/17   ┃');
+            disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
+            disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
+            disp('  当前目录：');
+            disp('   序号        名称');
+            for(i=1:1:itemCount)
+                fprintf('    %d          %s  \n',i,itemOutput{i,1});
+            end
+            disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
+            choice = input('  请输入序号选择 (数字): ','s');
+            choicePick = str2double(choice);
+            if isnan(choicePick)
+            else
+                break;
+            end
         end
-        disp('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
-        choice = input('  请输入序号选择 (数字): ');
-        while choice<1|choice>(items-1)
+        while choicePick<1|choicePick>(items-1)
             clc
             disp('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
             disp('┃                马特浩倪Matlab代码部署系统                    ┃');
